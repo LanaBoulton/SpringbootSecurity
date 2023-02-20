c0d6f9f6dc69e257e28acfb0c4c4b3fb82535cbe
Lana <gavrylovalova@gmail.com>
2023-02-20 15:12:54 -0800

    Initial commit

---
+++ src/main/java/io/getarrays/userService/domain/Role.java
@@ -0,0 +1,17 @@
+package io.getarrays.userService.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+
+@Entity @Data @NoArgsConstructor @AllArgsConstructor
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+
+}
